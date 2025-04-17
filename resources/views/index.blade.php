a33e2d05a73ccd78c9fbf4fdcdddf8aea14d32a2
EliuthL <eliuthsevilla@gmail.com>
2025-04-17 13:43:25 -0600

    Add SVG icon for Realmi branding

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -6,7 +6,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 gap-8">
             <div class="flex flex-col justify-center">
                 <h1 class="text-3xl md:text-5xl font-medium text-white">Compra con seguridad y Garantía</h1>
-                <p class="mt-8 text-white text-lg">Somos Yi-Cell una tienda de tecnología y articulos electrónicos con tienda física ubicada en la ciudad de Quilalí-Nueva Segovia</p>
+                <p class="mt-8 text-white text-lg">Somos <span class="font-bold">Yi-Cell</span> una tienda de tecnología y articulos electrónicos con tienda física ubicada en la ciudad de Quilalí-Nueva Segovia.</p>
                 <p class="mt-4 text-white text-lg">Mira nuestro catalogo en linea totalmente gratis y descubre un mundo de accesorios y dispositivos móviles que <span class="font-bold">Yi-Cell</span> te ofrece.</p>
                 <div clsass="">
                     <a href="https:www.jw.org" target="_blank" rel="noopener noreferrer">
@@ -35,34 +35,52 @@
                 <x-card img="/img/shopping-bag.svg"
                     alt="bolsa"
                     titulo="Catálogo Digital"
-                    descripcion="Puedes ver nuestro catálogo digital totalmente gratis."/>
+                    descripcion="Puedes ver nuestro catálogo digital totalmente gratis.">
+                    <a href="#" class="text-blue-600 hover:underline">Ver Catálogo&#8599;</a>
+                </x-card>
                 
                 <x-card img="/img/portfolio.svg"
                     alt="tienda"
                     titulo="Garantía en Compras"
-                    descripcion="Contamos con tienda física en la ciudad de Quilalí-Nueva Segovia"/>
+                    descripcion="Te brindamos garantía de tres meses y servicios postventa en tus compras.">
+                </x-card>
 
             </div>
 
-            <div class="">
-                <p class="text-center text-4xl fon text-gray-800 mt-14">
+            <div>
+                <p class="text-center text-4xl font-medium text-gray-800 mt-14">
                     Variedad en marcas
                 </p>
-                <div class="flex justify-center mt-8 gap-12 ">
-                    <img src="/img/xiaomi_icon.svg" alt="marcas" class="w-10" />
-                    <img src="/img/infinix_icon.svg" alt="marcas" class="w-20" />
-                    <img src="/img/apple_icon.png" alt="marcas" class="w-10" />
-                    <img src="/img/samsung_icon.svg" alt="marcas" class="w-20" />
-                    <img src="/img/playstation_icon.svg" alt="marcas" class="w-10" />
-                    <img src="/img/xbox_icon.png" alt="marcas" class="w-10" />
-                    <img src="/img/blu_icon.png" alt="marcas" class="w-16" />
+
+                <div class="mt-8">
+                    <div class="grid grid-cols-2 sm:grid-cols-4 md:flex md:justify-center md:gap-12 lg:gap-16 items-center">
+                        <img src="/img/xiaomi_icon.svg" alt="Xiaomi" class="w-10 mx-auto pt-6 md:pt-0" />
+                        <img src="/img/infinix_icon.svg" alt="Infinix" class="w-20 mx-auto pt-6 md:pt-0" />
+                        <img src="/img/apple_icon.png" alt="Apple" class="w-10 mx-auto pt-6 md:pt-0" />
+                        <img src="/img/samsung_icon.svg" alt="Samsung" class="w-20 mx-auto mt-6 md:pt-0" />
+                        <img src="/img/playstation_icon.svg" alt="PlayStation" class="w-10 mx-auto mt-6 md:pt-0" />
+                        <img src="/img/xbox_icon.png" alt="Xbox" class="w-10 mx-auto mt-6 md:pt-0" />
+                        <img src="/img/blu_icon.png" alt="Blu" class="w-16 mx-auto mt-6 md:pt-0" />
+                        <img src="/img/realmi_icon.svg" alt="Realmi" class="w-20 mx-auto mt-6 md:pt-0" />
+
+                    </div>
                 </div>
             </div>
+
         </div">
         
         
     </div>
-    
+
+    <div class="">
+        <div class="container w-full max-w-screen-xl mx-auto px-4 py-8 lg:pt-12">
+            <div>
+                <h1 class="text-3xl md:text-5xl font-medium text-gray-800 text-center">
+                    hola
+                </h1>
+            </div>
+        </div>
+    </div>
     
     <div class="bg-sky-700">
         <div class="w-full max-w-screen-xl mx-auto px-4 py-6 lg:py-8 ">
